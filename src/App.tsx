6d4f64f79f5debdf8fd5f347b6b225800ee56b5a
unknown <daniilbarilotti@gmail.com>
2025-02-04 12:42:31 +0100

    clone phone-catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
 import './App.scss';
+import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import React from 'react';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+export const App = () => (
+  <div className="App">
+    <h1 className="visually-hidden">Product Catalog</h1>
+    <Header />
+    <main>
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+);
